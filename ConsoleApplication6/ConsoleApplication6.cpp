2fa9020749685f65eb79d639f38cf634c31fe34f
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-13 21:02:14 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    double R1, R2, R3, R0;
+
+    std::cout << "Enter the values of the three resistances: \n";
+    std::cin >> R1 >> R2 >> R3;
+
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+
+    std::cout << "Answer is " << std::setprecision(3) << R0;
+}
